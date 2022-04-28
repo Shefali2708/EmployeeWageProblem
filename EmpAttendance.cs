8f4889c3931a9d89d9889859c7e93be7043f53a1
shefali <shefalis2708@gmail.com>
2022-04-28 19:20:00 +0530

    Check emp is present or absent

---
+++ EmpAttendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageProgram
+{
+    class EmpAttendance
+    {
+        //Check emp is present or abesent
+        //UC 1
+        public static void EmployeeAttendance()
+        {
+            //CONSTANT
+            int FULL_TIME = 1;
+            Random random = new Random();
+
+            //COMPUTATION
+            int empCheck = random.Next(0, 2);
+            if(empCheck == FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeeWageProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            EmpAttendance.EmployeeAttendance();
+        }
+    }
+}
